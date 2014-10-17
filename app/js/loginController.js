8259daa9bc4b1927156c0b83b21ab77400245c03
Diogo Pinto <diogojapinto@gmail.com>
2014-10-17 21:15:08 +0100

    restructured files tree

--- app/js/loginController.js
+++ app/js/loginController.js
@@ -2,13 +2,16 @@
   var app = angular.module('login', []);
 
   app.controller('LoginController', function(){
+      /* Controller that manages the login of an user */
     this.user = {};
 
     this.addUser = function (user) {
-
+        /* Submits the user information, in an attempt to login */
+        //TODO: get new page
     };
 
     this.resetForm = function() {
+        /* Cleans user input */
     	this.user = {};
     }
   });
