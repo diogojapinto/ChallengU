e37cd217feeb98c574f0d3b147e06c1300066723
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-10-17 18:30:15 +0100

    Server initialized and routes working

--- app/server/app.js
+++ app/server/app.js
@@ -9,9 +9,20 @@ app.use("/images", express.static(__dirname + '/../images'));
 
 app.use("/js", express.static(__dirname + '/../js'));
 
+app.use("/fonts", express.static(__dirname + '/../fonts'));
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, '../html', 'landing.html'));
 });
+
+app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, '../html', 'login.html'));
+});
+
+app.get("/postChallenge", function(req, res) {
+    res.sendFile(path.join(__dirname, '../html', 'challenge-submit.html'));
+});
+
 
 app.listen(8080, function() {
     console.log('listening on port:8080');
