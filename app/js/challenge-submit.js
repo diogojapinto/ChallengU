b50e01bd2016bf5286cf7d4dade29da2a895ac32
wso277 <oliveira.wilson93@gmail.com>
2014-10-19 16:44:23 +0100

    Almost done with creating a challenge

--- app/js/challenge-submit.js
+++ app/js/challenge-submit.js
@@ -1,49 +1,28 @@
 (function () {
-  var app = angular.module('challenge-submit', []);
+    var app = angular.module('challenge-submit', []);
 
-  app.controller('ChallengeSubmit', ['$scope','$http', 'Challenges',function ($scope,$http,Challenges) {
+    app.controller('ChallengeSubmit', ['$scope', '$http', 'Challenges', function ($scope, $http, Challenges) {
 
-    $scope.formData = {};
-    $scope.loading = true;
+        $scope.formData = {
+            name       : "",
+            category   : [],
+            type       : "video",
+            difficulty : 3,
+            description: ""};
+        $scope.loading = true;
 
+        $scope.createChallenge = function () {
+            $scope.loading = true;
+            if ($scope.formData.name != "" && $scope.formData.category.length >= 1) {
+                Challenges.create($scope.formData)
+            }
+        };
 
-    $scope.createChallenge = function(){
-      $scope.loading = true;
-      if($scope.formData != undefined){
-        Challenges.create($scope.formData)
-        .success(function(data){
-          $scope.loading = false;
-          $scope.formData = {};
-          alert("success");
-        })
-        .error(function(data){
-          alert("failed: " + data);
-        });
-      }
-    };
-    this.challenge = {
-      name      : "",
-      category  : [""],
-      type      : "",
-      difficulty: 3,
-      description: ""
-    };
+        //get categories
+        $scope.formData.challengeCategories = [];
+        Challenges.getCategories($scope.formData.challengeCategories);
 
-    //get categories
-    this.challengeCategories = [];
-    var categories = this.challengeCategories;
-    $http.post("/get-categories").
-    success(function (data, status, headers, config) {
-
-      for (i = 0; i < data.length; i++) {
-        categories[i] = data[i].name;
-      }
-    }).
-    error(function (data, status, headers, config) {
-      alert("fail");
-    });
-
-    this.challengeTypes = ['video', 'sound', 'text', 'photo'];
-  }]);
+        this.challengeTypes = ['sound', 'text', 'photo', 'video'];
+    }]);
 
 })();
--- app/js/challengeService.js
+++ app/js/challengeService.js
@@ -1,10 +1,28 @@
 angular.module('challenge-service', [])
 
-  .factory('Challenges', ['$http', function($http){
-    return{
-      create:function(challengeData){
-        alert("DIF = " + challengeData.difficulty);
-        return $http.post('/create-challenge', challengeData);
-      }
-    }
-  }]);
+    .factory('Challenges', ['$http', function ($http) {
+        return{
+            create       : function (challengeData) {
+                $http.post('/create-challenge', challengeData)
+                    .success(function (data) {
+                        $scope.loading = false;
+                        $scope.formData = {};
+                        alert("success");
+                    })
+                    .error(function (data) {
+                        alert("failed: " + data);
+                    });
+            },
+            getCategories: function (categories) {
+                $http.post("/get-categories")
+                    .success(function (data, status, headers, config) {
+                        for (i = 0; i < data.length; i++) {
+                            categories[i] = data[i].name;
+                        }
+                    }).
+                    error(function (data, status, headers, config) {
+                        alert("fail");
+                    });
+            }
+        }
+    }]);
