15ae38e9877f4957e11df6cc47fce95d03fe6390
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-10-28 10:08:24 +0000

    Unit tests

--- app/test/example.js
+++ app/test/example.js
@@ -1,5 +1,6 @@
 // load Unit.js module
 var test = require('unit.js');
+var test = require('should');
 // just for example of tested value
 var example = 'hello';
 // assert that example variable is a string
@@ -8,3 +9,80 @@ test.string(example);
 test.must(example).be.a.string();
 // or with assert
 test.assert(typeof example === 'string');
+
+describe('Account', function() {
+    it('should return error because the user does not exist', function (done) {
+        var user = {
+            username: 'blablabla',
+            password: 'testblabla'
+        };
+        // once we have specified the info we want to send to the server via POST verb,
+        // we need to actually perform the action on the resource, in this case we want to
+        // POST on /api/profiles and we want to send some info
+        // We do this using the request object, requiring supertest!
+        request(url)
+            .post('/login-user')
+            .send(user)
+            // end handles the response
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                // this is should.js syntax, very clear
+                res.should.have.status(400);
+                done();
+            });
+    });
+
+    it('should login correctly', function (done) {
+        var user = {
+            username: 'modd1',
+            password: 'passmod1'
+        };
+        // once we have specified the info we want to send to the server via POST verb,
+        // we need to actually perform the action on the resource, in this case we want to
+        // POST on /api/profiles and we want to send some info
+        // We do this using the request object, requiring supertest!
+        request(url)
+            .post('/login-user')
+            .send(user)
+            // end handles the response
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                // this is should.js syntax, very clear
+                res.should.have.status(200);
+                req.session.should.have.property(user);
+                done();
+            });
+    });
+});
+
+describe('Challenge', function() {
+    it('should return error because the the category field is empty', function (done) {
+        var challenge = {
+            name: 'blablabla',
+            category: [],
+            type: "video",
+            difficulty: "3",
+            description: "bla bla bla whiskas saquetas"
+        };
+        // once we have specified the info we want to send to the server via POST verb,
+        // we need to actually perform the action on the resource, in this case we want to
+        // POST on /api/profiles and we want to send some info
+        // We do this using the request object, requiring supertest!
+        request(url)
+            .post('/create-challenge')
+            .send(challenge)
+            // end handles the response
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                // this is should.js syntax, very clear
+                res.should.have.status(400);
+                done();
+            });
+    });
+});
