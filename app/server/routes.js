d1121c3156a4c6b82d1797e6ed653b217e40162b
wso277 <oliveira.wilson93@gmail.com>
2014-10-28 09:10:24 +0000

    Testing branches

--- app/server/routes.js
+++ app/server/routes.js
@@ -97,6 +97,7 @@ exports.start = function (app) {
                     res.status(200).send("OK");
                 });
             } else {
+
                 res.status(404).send("NOT OK");
             }
         });
