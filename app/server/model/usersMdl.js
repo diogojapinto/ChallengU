0f5e3c6b77eec55eed1f48fc8a1148c0d8c56683
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-10-30 16:47:39 +0000

    Search upgrade, now showing users, numberof comments and ordering by them inc

--- app/server/model/usersMdl.js
+++ app/server/model/usersMdl.js
@@ -7,3 +7,7 @@ exports.getUser = function (username, callback) {
 exports.register = function (username, password, name, email, work, hometown, userType, userState, callback) {
     db.query("INSERT INTO RegisteredUser (userID, username, pass, name, email, work, hometown, lastFreePoints, xp, userType, userState) VALUES (DEFAULT,"+"'"+username+"'"+","+"'"+password+"'"+","+"'"+name+"'"+","+"'"+email+"'"+","+"'"+work+"'"+","+"'"+hometown+"'"+",DEFAULT,DEFAULT,"+"'"+userType+"'"+","+"'"+userState+"'"+")", [], callback);
 };
+
+exports.getUserByID = function(userID,callback) {
+    db.query("SELECT * from RegisteredUser WHERE userID = " + userID, [], callback);
+};
--- app/server/routes.js
+++ app/server/routes.js
@@ -70,10 +70,6 @@ exports.listen = function (app) {
         res.render('dummy-search.ejs', {title: 'Search challenge'});
     });
 
-    app.post("/search-challenge", function (req, res) {
-        challengeFn.searchChallenges(req.body.searchValue,res);
-    });
-
     app.post("/register", function (req, res) {
         console.log(req.body);
         userFn.registerUser(req.body,res);
