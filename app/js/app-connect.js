17a57a068e01741f7ee9cccf93b2ff3deb2b719e
Diogo Pinto <diogojapinto@gmail.com>
2014-11-10 21:41:37 +0000

    solved all the problems of humanity... and of connect-app

--- app/js/app-connect.js
+++ app/js/app-connect.js
@@ -2,7 +2,6 @@
     var app = angular.module('connect-app', ['login-app', 'register-app']);
 
     angular.element(document).ready(function() {
-        console.log(angular.element('a[title]'));
         angular.element('a[title]').tooltip();
     });
-});
+})();
