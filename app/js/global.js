9e6b5b1823b4865963679b61b318b694c2e6bbfe
Diogo Pinto <diogojapinto@gmail.com>
2014-11-19 18:15:31 +0000

    switched minified/production versions to normal/development ones

--- app/js/global.js
+++ app/js/global.js
@@ -2,25 +2,4 @@
     angular.element(document).ready(function () {
         angular.element('a[title]').tooltip();
     });
-
-    document.querySelector('.material-design-hamburger__icon').addEventListener(
-        'click',
-        function() {
-            var child;
-
-            document.body.classList.toggle('background--blur');
-            this.parentNode.nextElementSibling.classList.toggle('menu--on');
-
-            angular.element('.menu span').fadeToggle();
-
-            child = this.childNodes[1].classList;
-
-            if (child.contains('material-design-hamburger__icon--to-arrow')) {
-                child.remove('material-design-hamburger__icon--to-arrow');
-                child.add('material-design-hamburger__icon--from-arrow');
-            } else {
-                child.remove('material-design-hamburger__icon--from-arrow');
-                child.add('material-design-hamburger__icon--to-arrow');
-            }
-        });
 })();
---
+++ app/js/navbar.js
@@ -0,0 +1,24 @@
+(function () {
+    document.querySelector('.material-design-hamburger__icon').addEventListener(
+        'click',
+        function() {
+            var child;
+
+            document.body.classList.toggle('background--blur');
+            this.parentNode.nextElementSibling.classList.toggle('menu--on');
+
+            if (angular.element(window).width >= 768) {
+                angular.element('.menu span').fadeToggle();
+            }
+
+            child = this.childNodes[1].classList;
+
+            if (child.contains('material-design-hamburger__icon--to-arrow')) {
+                child.remove('material-design-hamburger__icon--to-arrow');
+                child.add('material-design-hamburger__icon--from-arrow');
+            } else {
+                child.remove('material-design-hamburger__icon--from-arrow');
+                child.add('material-design-hamburger__icon--to-arrow');
+            }
+        });
+})();
