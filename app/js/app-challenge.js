1983d8cc166176e718ef58aca321b27b3d6e781f
wso277 <oliveira.wilson93@gmail.com>
2014-12-21 16:26:18 +0000

    comments almost finished

--- app/js/app-challenge.js
+++ app/js/app-challenge.js
@@ -16,6 +16,20 @@
         }
     }]);
 
+    app.controller('CommentController', ['$scope', '$http', function($scope, $http) {
+        $scope.content = "";
+        $scope.addComment = function(user, challengeid) {
+            console.log(user);
+            $http.post('/add-comment', {user: user, content: $scope.content, challengeid: challengeid})
+                .success(function(data) {
+                    angular.element("#comments").prepend('<li class="list-group-item"> <div class="row"> <div class="col-xs-2 col-md-1"> <img src="http://placehold.it/80" class="img-circle img-responsive" alt=""/></div> <div class="col-xs-10 col-md-11"> <div> <div class="mic-info">By: <a href="/profile/' + user + '"> ' + user + ' </a> </div> </div> <div class="comment-text"> ' + $scope.content + '%> </div> </div> </div> </li>');
+                })
+                .error(function(data) {
+                    console.log("error");
+                });
+        };
+    }]);
+
     app.factory('Proof', ['$http', '$window', function ($http, $window) {
         return{
             /**
