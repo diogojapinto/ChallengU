0ac1313c1bf20436611d5f709e84d70568ac3e60
Diogo Pinto <diogojapinto@gmail.com>
2014-12-22 23:18:55 +0000

    small fixes

--- app/server/loginConfig/auth.js
+++ app/server/loginConfig/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth' : {
       'clientID': '555365347933098',
       'clientSecret': '3377efc2ae626cd15a0a887841765c68',
-      'callbackURL': 'http://localhost:8081/auth/facebook/callback'
+      'callbackURL': 'http://178.62.101.158:8081/auth/facebook/callback'
   }
 };
